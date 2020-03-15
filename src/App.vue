4ad8bfcc6b1bd94c5747b4cfe80535d84a2cb80c
TigranMnjoyan96 <mnjoyan1996@gmail.com>
2020-03-16 00:21:56 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="container">
+      <h2>hello</h2>
+
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input
+          type="text"
+          v-model.trim="carName"
+          id="name"
+          class="form-control"
+        />
+      </div>
+      <div class="form-group">
+        <label for="year">Year</label>
+        <input
+          type="text"
+          id="year"
+          v-model.number="year"
+          class="form-control"
+        />
+      </div>
+      <button class="btn btn-success" @click="createCar">
+        Create Car
+      </button>
+      <button class="btn btn-primary ml-3" @click="loadCars">Load Cars</button>
+      <hr />
+      <ul class="list-group">
+        <li class="list-group-item" v-for="(item, k) in cars" :key="k">
+          <strong>{{ item.carName }}</strong> - {{ item.year }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      carName: "",
+      year: 2013,
+      cars: [],
+      resource: null
+    };
+  },
+  methods: {
+    createCar() {
+      const car = {
+        carName: this.carName,
+        year: this.year
+      };
+      // this.$http.post("http://localhost:3000/cars", car).then(response => {
+      //   return response.json().then(newCar => {
+      //     console.log(newCar);
+      //   });
+      // });
+
+      this.resource.save({}, car);
+    },
+    loadCars() {
+      // this.$http.get("http://localhost:3000/cars").then(response => {
+      //   response.json().then(cars => {
+      //     this.cars = cars;
+      //   });
+      // });
+      this.resource
+        .get()
+        .then(response => response.json().then(cars => (this.cars = cars)));
+    }
+  },
+  created() {
+    this.resource = this.$resource("cars");
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
+import VueResource from "vue-resource";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
+Vue.use(VueResource);
+Vue.http.options.root = "http://localhost:3000/";
 
 new Vue({
   render: h => h(App)
